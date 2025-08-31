88b75ea64d34c3bc7ff5bf184982e38e58ad9028
Priyanshu Agrawal <priyanshuagrawal303@gmail.com>
2025-08-31 11:12:31 +0530

    Update VideoMeet.jsx

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -530,9 +530,9 @@ import MicOffIcon from '@mui/icons-material/MicOff'
 import ScreenShareIcon from '@mui/icons-material/ScreenShare';
 import StopScreenShareIcon from '@mui/icons-material/StopScreenShare'
 import ChatIcon from '@mui/icons-material/Chat'
-// import server from '../environment';
+import server from '../envirement';
 
-const server_url = "http://localhost:8000";
+const server_url = server;
 
 var connections = {};
 
